54031eab017362ac40070b168956787598e8d421
Ynda Jas <yndajas@gmail.com>
2024-05-17 19:42:02 +0100

    Add end-to-end tests
    This adds Playwright and some very basic end-to-end tests. The goal here
    is to get Renovate auto-merging dependencies updates that don't break
    anything. These tests will allow for a basic check that can be set as
    required before Renovate merges anything

---
+++ e2e/website.spec.js
@@ -0,0 +1,39 @@
+// @ts-check
+const { test, expect } = require('@playwright/test');
+
+test('index page renders correctly', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page).toHaveTitle("Ynda Jas");
+  await expect(page.locator("h1")).toHaveText("Hello, world!");
+  await expect(page.locator("h2")).toHaveText([
+    "Current things",
+    "Things that I'd like to be more current",
+    "Sometimes things",
+    "Things that once were",
+    "Things that are trival"
+  ]);
+  await expect(page.locator("img")).toHaveCount(5);
+  await expect(page.locator("footer p")).toHaveCount(3);
+});
+
+test('Nihongo page renders correctly', async ({ page }) => {
+  await page.goto('/nihongo');
+
+  await expect(page).toHaveTitle("Ynda Jas");
+  await expect(page.locator("h1")).toHaveText("My Japanese language learning resources");
+  await expect(page.locator("main p")).toHaveText("List to follow");
+  await expect(page.locator("footer p")).toHaveCount(3);
+});
+
+test('writing page renders correctly', async ({ page }) => {
+  await page.goto('/writing');
+
+  await expect(page).toHaveTitle("Ynda Jas");
+  await expect(page.locator("h1")).toHaveText("Things I've written");
+  await expect(page.locator("main p")).toHaveText([
+    "Links to writings elsewhere",
+    "Blog posts"
+  ]);
+  await expect(page.locator("footer p")).toHaveCount(3);
+});
